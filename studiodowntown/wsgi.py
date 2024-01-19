23663a93085ea5368e86b77f0ec82dbdb7bc3202
noisenetwork <noisenetwork@users.noreply.github.com>
2024-01-19 12:49:23 -0800

    initial upload

---
+++ studiodowntown/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for studiodowntown project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studiodowntown.settings')
+
+application = get_wsgi_application()
